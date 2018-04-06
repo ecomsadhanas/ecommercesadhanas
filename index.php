21bb69a73d1eb9ff2eb32c171ede33f8cab78bda
beniwn <beni.witanto@gmail.com>
2018-04-06 22:04:06 +0700

    index  pertama

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Title</title>
+  </head>
+  <body>
+      <table>
+        <tr>
+          <td>No.</td>
+          <td>Nama.</td>
+          <td>Alamat.</td>
+        </tr>
+      </table>
+  </body>
+</html>
